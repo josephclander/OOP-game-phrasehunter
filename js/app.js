9d5082d8f19ca502ff45d55c15c623db9e036996
Joseph Lander <josephclander@gmail.com>
2021-03-13 06:39:05 +0000

    Add keyboard listener

--- js/app.js
+++ js/app.js
@@ -7,9 +7,24 @@ document.querySelector('#btn__reset').addEventListener('click', function () {
   game.startGame();
 });
 
+/**
+ * Event listener for ONSCREEN keys
+ */
 document.querySelectorAll('.key').forEach((key) => {
   key.addEventListener('click', function (event) {
     const button = event.target;
     game.handleInteraction(button);
+  });
+});
+
+/**
+ * Event listener for KEYBOARD keys
+ * make response match onscreen keys
+ */
+document.addEventListener('keydown', function (event) {
+  const keyPressed = event.key;
+  const onscreenButtons = document.querySelectorAll('.key');
+  onscreenButtons.forEach((button) => {
+    if (button.textContent === keyPressed) game.handleInteraction(button);
   });
 });
